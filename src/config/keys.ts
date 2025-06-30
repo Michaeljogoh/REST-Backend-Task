4a89b1fc998a524a30078e807e8d73548681500d
Michael Jogoh <87239482+Michaeljogoh@users.noreply.github.com>
2025-06-30 12:05:09 +0100

    Update keys.ts

--- src/config/keys.ts
+++ src/config/keys.ts
@@ -1,4 +1,4 @@
 export default {
-    MongoURI: 'mongodb://localhost:27017/Pay-Ever',
-    AMQPS_URL:'amqps://wqqtmnnf:hP43JSna3LMVhKMS9xPFkQIMYcsUGtUc@rattlesnake.rmq.cloudamqp.com/wqqtmnnf'
+    MongoURI: '',
+    AMQPS_URL:''
 }
